5a5dc0a7503062535553432891316724ea438aca
buzzer <buzzer@it7.com.ua>
2016-10-21 11:57:55 +0300

    Add ability expand session in MyAgenda

--- app/components/my-agenda-list.component.ts
+++ app/components/my-agenda-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {PluginConfig} from "../services/plugin.config";
 import {MyAgendaService} from "../services/my-agenda.service";
+import {AgendaSession} from "../models/agenda-session";
 
 @Component({
     selector: 'my-agenda-list',
@@ -10,4 +11,10 @@ export class MyAgendaListComponent {
     @Input() public myAgenda: MyAgendaService;
 
     constructor() {}
+
+    // Component events
+
+    public onExpandClick(session:AgendaSession){
+        (session as any)._expanded = !(session as any)._expanded;
+    }
 }
